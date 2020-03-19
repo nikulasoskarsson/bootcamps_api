b762c6f019bb72de1241327b216fac817cf90ff0
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-03-19 11:59:47 +0000

    Connecting To the database and colors added to console logs

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Middlewares imports
-const logger = require('./middleware/logger');
-//Route files
-const bootcamps = require('./routes/bootcamps');
-
+const colors = require('colors');
+const connectDB = require('./config/db'); // Bringing in the database connection function
 //Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,7 +22,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port {PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port {PORT}`.blue.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
